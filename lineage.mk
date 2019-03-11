dfbcaa309f73a7e99f7c676a3ba7bea98d6657e7
Ed Porter <enp@acm.org>
2019-03-11 15:30:18 +0100

    Adapt configs to match flo, so lineage-15.1 will build.

--- lineage.mk
+++ lineage.mk
@@ -5,10 +5,10 @@ TARGET_SCREEN_WIDTH := 1200
 TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
 
 # Inherit telephony stuff
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/lineage/config/telephony.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
